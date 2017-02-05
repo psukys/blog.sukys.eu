581cfc86bc32323a2e02e70146be645afde351b0
Paulius Šukys <paul.sukys@gmail.com>
2017-02-05 16:20:19 +0100

    Initial version for a simple front page: using bing day image as a background through a yahoo cors proxy

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function load_background_image(data) {
+    var img = data.query.results.json.images;
+    $("body").css("background-image", "url(http://www.bing.com" + img.url + ")");
+    // add copyring attribution at footer
+    $("div.mastfoot > div.inner").text("Image copyright: " + img.copyright);
+}
+
+
+$(document).ready(function() {
+    // Load up today's image from bing front page
+    // Implementation using https://blenderviking.github.io/2016/11/27/JavaScript-How-to-call-a-remote-service-with-using-a-JSONP-Proxy/
+    // Using Yahoo query language as a CORS proxy
+    var url = 'http://query.yahooapis.com/v1/public/yql' +
+        '?q=' + encodeURIComponent('select * from json where url=@url') +
+        '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1') +
+        '&format=json&callback=?';
+    $.getJSON(url, load_background_image);
+    // add copyright attribution at footer
+});

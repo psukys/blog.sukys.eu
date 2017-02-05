ea24265b8fed48608ac3b8f6656e83cbe883f5f4
Paulius Šukys <paul.sukys@gmail.com>
2017-02-05 16:22:35 +0100

    Added https for yahoo query language request as it's blocked, given github is https'ish

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     // Load up today's image from bing front page
     // Implementation using https://blenderviking.github.io/2016/11/27/JavaScript-How-to-call-a-remote-service-with-using-a-JSONP-Proxy/
     // Using Yahoo query language as a CORS proxy
-    var url = 'http://query.yahooapis.com/v1/public/yql' +
+    var url = 'https://query.yahooapis.com/v1/public/yql' +
         '?q=' + encodeURIComponent('select * from json where url=@url') +
         '&url=' + encodeURIComponent('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1') +
         '&format=json&callback=?';
